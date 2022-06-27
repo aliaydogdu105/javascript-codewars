4666a0bb582082122829c923830cc9022f0623b8
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-06-28 00:15:50 +0300

    readme and test

--- find-the-odd-number/solution.js
+++ find-the-odd-number/solution.js
@@ -2,3 +2,6 @@ function findOdd(A) {
   let counts = A.reduce((p, n) => ((p[n] = ++p[n] || 1), p), {});
   return +Object.keys(counts).find((k) => counts[k] % 2) || undefined;
 }
+
+let doTest = (20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5);
+console.log(doTest); // 5 is odd(3 pieces), others are even
